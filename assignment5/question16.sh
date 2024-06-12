536f980d53457394dc4945c41d646e08c2c5b206
d4_83647 <shejulshubhamdeshmukh@gmail.com>
2024-06-12 18:59:44 +0530

    question 16

---
+++ assignment5/question16.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Write a shell script to display welcome message to the user along with contents of his home directory. Ensure that shell script will execute automatically when user login to the shell. (Make entry of your shell script into .bashrc file into your home directory).
+
+echo "Welcome $USER"
+echo "Contents In Home Directory : "
+ls $HOME
